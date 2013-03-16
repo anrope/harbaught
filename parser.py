9ff0c1425637bb8c9791629aa7839b0e2b6842c0
Andy Pellett <anrope@gmail.com>
2013-03-16 14:57:45 -0400

    Parse csv into nicely named dict

--- parser.py
+++ parser.py
@@ -1,6 +1,29 @@
 import csv
 
+plays = []
+
 with open('foozball_excerpt_2011.csv', 'rb') as foozball:
     reader = csv.reader(foozball)
     for row in reader:
-        print 'lol wat', row[-1]
+        #gameid,qtr,min,sec,off,def,down,togo,ydline,description,offscore,defscore,season
+        named = {
+            'game_slug': row[0],
+            'quarter': row[1],
+            'minutes': row[2],
+            'seconds': row[3],
+            'o_team': row[4],
+            'd_team': row[5],
+            'down': row[6],
+            'to_go': row[7],
+            'yard_line': row[8],
+            'play': row[9],
+            'o_score': row[10],
+            'd_score': row[11],
+            'season': row[12]
+        }
+
+        plays.append(named)
+
+
+print len(plays)
+print plays[5]
