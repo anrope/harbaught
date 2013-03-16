986b19fe12b57051fc1d735a8a9296885664203f
Andy Pellett <anrope@gmail.com>
2013-03-16 15:19:17 -0400

    Add conversion to neural net input

---
+++ numerify.py
@@ -0,0 +1,50 @@
+def time_to_nn(minutes, second):
+    if minutes < 2:
+        return 0
+    else:
+        return 1
+
+def score_to_nn(o_score, d_score):
+    margin = o_score - d_score
+    if margin < 3:
+        return 0
+    elif margin < 7:
+        return 1
+    elif margin < 10:
+        return 2
+    elif margin < 14:
+        return 3
+    elif margin < 17:
+        return 4
+    else:
+        return 5
+
+def quarter_to_nn(quarter):
+    if quarter == 1:
+        return 0
+    elif quarter == 2:
+        return 1
+    elif quarter == 3:
+        return 0
+    elif quarter == 4:
+        return 2
+    else:
+        return 2
+
+def play_to_nn(play):
+    if play == 'RUN':
+        return 0
+    elif play == 'PASS':
+        return 1
+    elif play == 'PUNT':
+        return 2
+    elif play == 'FG':
+        return 3
+    elif play == 'KNEEL':
+        return 4
+    elif play == 'PAT':
+        return 5
+    elif play == 'SPIKE':
+        return 6
+    elif play == 'PENALTY':
+        return 7
--- parser.py
+++ parser.py
@@ -24,6 +24,15 @@ with open('foozball_excerpt_2011.csv', 'rb') as foozball:
 
         plays.append(named)
 
-
 print len(plays)
 print plays[5]
+
+for play in plays:
+    nn_input = {
+        'score': score_to_nn(play['o_score'], play['d_score']),
+        'quarter': quarter_to_nn(play['quarter']),
+        'time': time_to_nn(play['time']),
+        'play': play_to_nn(play['play'])
+    }
+
+    print nn_input
