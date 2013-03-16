ca952a78d612bd15be90dfac65463e9adcbd71f8
agibson <art@embed.ly>
2013-03-16 15:11:53 -0400

    working playmker

--- playmaker.py
+++ playmaker.py
@@ -6,14 +6,14 @@ Make and categorize plays.
 import re
 
 # PLAY TYPES
-RUN = 'RUN',
-PASS = 'PASS',
-PUNT = 'PUNT',
+RUN = 'RUN'
+PASS = 'PASS'
+PUNT = 'PUNT'
 FG = 'FIELD_GOAL'
 
 # PLAY REGEX
 RUN_RE = re.compile('Run\sformation|' \
-    '(left|right\sguard|tackle)|' \
+    '((left|right)\s(guard|tackle))|' \
     'up\sthe\smiddle')
 PASS_RE = re.compile('pass')
 PUNT_RE = re.compile('punt')
@@ -24,13 +24,13 @@ def get_play_type(play):
     Takes play text and returns the type
     of play RUN,PASS, etc.
     """
-    if PASS_RE.match(play):
+    if PASS_RE.search(play):
         return PASS
-    elif PUNT_RE.match(play):
+    elif PUNT_RE.search(play):
         return PUNT
-    elif FG_RE.match(play):
+    elif FG_RE.search(play):
         return FG
-    elif RUN_RE.match(play):
+    elif RUN_RE.search(play):
         return RUN
     else:
         raise Exception('COULD NOT FIND PLAY TYPE:%s' % play)
