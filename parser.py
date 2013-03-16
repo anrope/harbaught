c972abc32df8d9d45595c9a062fc648b2f2d5390
Andy Pellett <anrope@gmail.com>
2013-03-16 15:27:59 -0400

    Create neural net input data structure

--- parser.py
+++ parser.py
@@ -1,5 +1,6 @@
 import csv
 import playmaker
+import numerify
 
 plays = []
 
@@ -9,19 +10,26 @@ with open('foozball_excerpt_2011.csv', 'rb') as foozball:
         #gameid,qtr,min,sec,off,def,down,togo,ydline,description,offscore,defscore,season
         named = {
             'game_slug': row[0],
-            'quarter': row[1],
-            'minutes': row[2],
-            'seconds': row[3],
+            'quarter': int(row[1]),
+            'minutes': int(row[2]),
+            'seconds': int(row[3]),
             'o_team': row[4],
             'd_team': row[5],
             'down': row[6],
             'to_go': row[7],
-            'yard_line': row[8],
+            'yard_line': int(row[8]),
             'play': row[9],
-            'o_score': row[10],
-            'd_score': row[11],
+            'o_score': int(row[10]),
+            'd_score': int(row[11]),
             'season': row[12]
         }
+
+        if not named['down']:
+            named['down'] = None
+
+        if not named['to_go']:
+            named['to_go'] = None
+
         try:
             named['play_type'] = playmaker.get_play_type(named['play'])
         except Exception, e:
@@ -34,10 +42,10 @@ print plays[5]
 
 for play in plays:
     nn_input = {
-        'score': score_to_nn(play['o_score'], play['d_score']),
-        'quarter': quarter_to_nn(play['quarter']),
-        'time': time_to_nn(play['time']),
-        'play': play_to_nn(play['play'])
+        'score': numerify.score_to_nn(play['o_score'], play['d_score']),
+        'quarter': numerify.quarter_to_nn(play['quarter']),
+        'time': numerify.time_to_nn(play['minutes'], play['seconds']),
+        'play': numerify.play_to_nn(play['play'])
     }
 
     print nn_input
